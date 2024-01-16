8bf9f206b3efb5e88ef5827b12570c73ce5776aa
Tania Petruk <127411602+tanightt@users.noreply.github.com>
2024-01-16 17:39:42 +0200

    Created global styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    screens: {
+      xs: "320px",
+      sm: "576px",
+      md: "768px",
+      lg: "976px",
+      xl: "1280px",
+    },
+    colors: {
+      orange: "#E9672B",
+      black: "#000000",
+      pink: "#F6CFCA",
+      header: "#FBF8F5",
+      footer: "#F3F0EC",
+      nav: "#F0E5DA",
+    },
+    fontFamily: {
+      convergence: ["var(--font-convergence)"],
+      marck: ["var(--font-marck)"],
+      imprima: ["var(--font-imprima)"],
+      livvic: ["var(--font-livvic)"],
     },
   },
   plugins: [],
-}
+};
