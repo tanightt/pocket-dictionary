b22f05c9b5d34b648104a5af950b070afe5c0df1
Tania Petruk <127411602+tanightt@users.noreply.github.com>
2024-01-19 14:36:09 +0200

    Created container, eslintrc

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
     <main>
-      <h1 className="bg-pink">Hello</h1>
+      <h1 className="container bg-pink lg:bg-orange">Hello</h1>
+      <p className="container sm:bg-orange font-livvic">My friend</p>
       <button className="btn">Hi</button>
     </main>
   );
